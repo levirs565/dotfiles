3af3264eaf20fcdc711bd899f49e23bdfebca823
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2021-02-19 19:29:06 +0700

    update-colors: add neovim support - use `set background=` cmd - write cmd to file in home for new neovim instance - send cmd throught neovim module - list nvim instance using named pipe

--- scripts/update-colors.js
+++ scripts/update-colors.js
@@ -1,6 +1,13 @@
+// prevent nvim module to log debug
+process.env.NVIM_NODE_LOG_LEVEL = 'error'
+process.env.ALLOW_CONSOLE = 'yes'
+
 const yaml = require('yaml')
 const fs = require('fs')
 const path = require('path')
+const os = require('os')
+const nvim = require('neovim')
+const net = require('net')
 
 let theme
 if (new Date().getHours() >= 18)
@@ -20,3 +27,21 @@ let batTheme = 'gruvbox'
 if (theme == 'light') batTheme += '-' + theme
 batConfig = batConfig.replace(/(--theme)="[^"]+"/, `$1="${batTheme}"`)
 fs.writeFileSync(batFile, batConfig)
+
+const nvimColorFile = path.join(os.homedir(), '.nvim_color')
+const nvimCmd = `set background=${theme}`
+fs.writeFileSync(nvimColorFile, nvimCmd)
+
+// Windows names pipe root
+const ipcDir = '\\\\.\\pipe\\\\'
+const nvimIpcs = fs.readdirSync(ipcDir).filter(name => name.startsWith('nvim'))
+for (ipc of nvimIpcs) {
+  const socket = net.connect(`${ipcDir}${ipc}`)
+  const client = nvim.attach({ 
+    reader: socket,
+    writer: socket
+  })
+  client.command(nvimCmd).then(() => {
+    socket.end()
+  })
+}
