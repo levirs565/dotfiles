455e94abb8f116d5915972e3e2ae767dc8e506b5
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2021-03-04 16:31:12 +0700

    update-color: use async await

--- scripts/update-colors.js
+++ scripts/update-colors.js
@@ -2,11 +2,9 @@
 process.env.NVIM_NODE_LOG_LEVEL = 'error'
 process.env.ALLOW_CONSOLE = 'yes'
 
-const yaml = require('yaml')
-const fs = require('fs')
+const fs = require('fs').promises
 const path = require('path')
 const os = require('os')
-const nvim = require('neovim')
 const net = require('net')
 
 let theme
@@ -14,34 +12,53 @@ if (new Date().getHours() >= 18)
   theme = 'dark'
 else theme = 'light'
 
-const alacrittyConfig = path.join(__dirname, '../alacritty/alacritty.yml')
-const doc = yaml.parseDocument(fs.readFileSync(alacrittyConfig, 'utf8'))
-const alias = doc.anchors.createAlias(doc.anchors.getNode(theme))
+async function alacritty() {
+  const yaml = require('yaml')
+  const file = path.join(__dirname, '../alacritty/alacritty.yml')
 
-doc.set('colors', alias)
-fs.writeFileSync(alacrittyConfig, doc.toString())
+  const content = await fs.readFile(file, 'utf8')
+  const doc = yaml.parseDocument(content)
+  const alias = doc.anchors.createAlias(doc.anchors.getNode(theme))
 
-const batFile = path.join(__dirname, '../bat/config')
-let batConfig = fs.readFileSync(batFile, 'utf8')
-let batTheme = 'gruvbox'
-if (theme == 'light') batTheme += '-' + theme
-batConfig = batConfig.replace(/(--theme)="[^"]+"/, `$1="${batTheme}"`)
-fs.writeFileSync(batFile, batConfig)
-
-const nvimColorFile = path.join(os.homedir(), '.nvim_color')
-const nvimCmd = `set background=${theme}`
-fs.writeFileSync(nvimColorFile, nvimCmd)
-
-// Windows names pipe root
-const ipcDir = '\\\\.\\pipe\\\\'
-const nvimIpcs = fs.readdirSync(ipcDir).filter(name => name.startsWith('nvim'))
-for (ipc of nvimIpcs) {
-  const socket = net.connect(`${ipcDir}${ipc}`)
-  const client = nvim.attach({ 
-    reader: socket,
-    writer: socket
-  })
-  client.command(nvimCmd).then(() => {
-    socket.end()
-  })
+  doc.set('colors', alias) 
+  await fs.writeFile(file, doc.toString())
 }
+
+async function bat() {
+  const file = path.join(__dirname, '../bat/config')
+  let config = await fs.readFile(file, 'utf8')
+
+  let batTheme = 'gruvbox'
+  if (theme == 'light') batTheme += '-' + theme
+
+  config = config.replace(/(--theme)="[^"]+"/, `$1="${batTheme}"`)
+  await fs.writeFile(file, config)
+}
+
+async function nvim() {
+  const colorFile = path.join(os.homedir(), '.nvim_color')
+  const cmd = `set background=${theme}`
+  await fs.writeFile(colorFile, cmd)
+
+  // Windows names pipe root
+  const ipcDir = '\\\\.\\pipe\\\\'
+  const ipcList = (await fs.readdir(ipcDir))
+    .filter(name => name.startsWith('nvim'))
+
+  if (ipcDir.length == 0) return
+
+  const nvim = require('neovim')
+  for (ipc of ipcList) {
+    const socket = net.connect(`${ipcDir}${ipc}`)
+    const client = nvim.attach({ 
+      reader: socket,
+      writer: socket
+    })
+    await client.command(cmd)
+    socket.end()
+  }
+}
+
+alacritty()
+bat()
+nvim()
