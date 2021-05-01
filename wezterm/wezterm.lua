6ecb9f4b6d275c96bfc2b30c010f25aecf4715b3
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2021-05-01 11:01:35 +0700

    weztern: remove unused setting

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,7 +3,6 @@ local wezterm = require 'wezterm';
 return {
   font = wezterm.font("JetBrainsMono NF"),
   font_size = 11.0,
-  force_reverse_video_cursor = true,
   color_scheme = "Gruvbox Light",
   default_prog = {"pwsh"},
   default_cwd = os.getenv("HOMEDRIVE") .. os.getenv("HOMEPATH"),
