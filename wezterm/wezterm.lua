d15eec8bedacad93001ef7ea050bf27d00f0f77f
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2021-05-01 09:32:24 +0700

    add wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,15 @@
+local wezterm = require 'wezterm';
+
+return {
+  font = wezterm.font("JetBrainsMono NF"),
+  font_size = 11.0,
+  force_reverse_video_cursor = true,
+  color_scheme = "Gruvbox Light",
+  default_prog = {"pwsh"},
+  default_cwd = "C:\\Users\\levir",
+  foreground_text_hsb = {
+    hue = 1.0,
+    saturation = 1.0,
+    brightness = 1.0,
+  },
+}
