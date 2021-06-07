e75a022d9152b7ecbcc6427783ae11e12d5b139d
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2021-06-07 15:44:24 +0700

    add msys to wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,6 +4,8 @@ local file = io.open(wezterm.home_dir .. "/.wezterm_color", "r")
 local color = file:read("*l")
 file:close()
 
+local msys2bash = { "C:\\msys64\\usr\\bin\\bash", "--login" }
+
 return {
   font = wezterm.font("JetBrainsMono NF"),
   font_size = 11.0,
@@ -19,6 +21,20 @@ return {
     {
       label = "Neovim",
       args = { "nvim" }
+    },
+    {
+      label = "MSYS2",
+      args = msys2bash,
+      set_environment_variables = {
+        MSYSTEM = "MSYS"
+      }
+    },
+    {
+      label = "MSYS2 MINGW64",
+      args = msys2bash,
+      set_environment_variables = {
+        MSYSTEM = "MINGW64"
+      }
     }
   },
   keys = {
@@ -28,5 +44,7 @@ return {
       mods = "CTRL",
       action = wezterm.action{SendString="\x1b\r"}
     }
-  }
+  },
+  tab_bar_at_bottom = true,
+  force_reverse_video_cursor = true
 }
